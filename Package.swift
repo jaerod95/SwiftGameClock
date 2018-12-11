2509440d830fb79dfd5efda61b547ea1f6f5dadf
Jason Rodriguez <jason@frenteventures.com>
2018-12-11 15:57:14 -0700

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,6 @@
+import PackageDescription
+
+let package = Package(
+    name: "SwiftGameClock"
+    dependencies: [],
+    exclude: ["Sources/info.plist", "Sources/SwiftGameClock.h", "SwiftGameClockTests"]
